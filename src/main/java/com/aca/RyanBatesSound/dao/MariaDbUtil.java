3cbcf718d8af5c3138dab6a4d0ffdb5dea14555b
Astaroth851 <142568537+Astaroth851@users.noreply.github.com>
2024-05-07 17:28:54 -0500

    Update MariaDbUtil.java

--- src/main/java/com/aca/RyanBatesSound/dao/MariaDbUtil.java
+++ src/main/java/com/aca/RyanBatesSound/dao/MariaDbUtil.java
@@ -1,38 +1,47 @@
 package com.aca.RyanBatesSound.dao;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
 
+
 public class MariaDbUtil {
 
 	private static String connectionUrl = 
-			"jdbc:mariadb://localhost:3306/soundmovies?user=root&password=password";
+			"jdbc:mysql://";
 	
-	public static Connection getConnection() {
-		Connection connection = null;
-		try {
-			Class.forName("org.mariadb.jdbc.Driver");
-			connection = DriverManager.getConnection(connectionUrl);
-		} catch (ClassNotFoundException e) {
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+        	Class.forName("com.mysql.cj.jdbc.Driver");
+        	 String dbHost = System.getenv("DATABASE_HOST");
+             String dbUsername = System.getenv("DATABASE_USERNAME");
+             String dbPassword = System.getenv("DATABASE_PASSWORD");
+             String dbName = System.getenv("DATABASE");
+             String urlWithCredentials = connectionUrl + dbHost + "?user=" + dbUsername + "&password=" + dbPassword;
+
+			connection = DriverManager.getConnection(urlWithCredentials);
+        } catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-			
-		return connection;
-	}
-	
-	public static void main (String[] args) throws SQLException {
-		Connection connection = MariaDbUtil.getConnection();
+        } catch (SQLException e) {
+            // Print the exception for diagnosis
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void main(String[] args) {
+    	Connection connection = MariaDbUtil.getConnection();
 		if (null != connection) {
 			System.out.println("A real connection");
-		
 		} else 
-			
 			System.out.println("Help. Connection is null.");
 		}
-	}
+        
+    }
 
+
+        
+    
+    		
