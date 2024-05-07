99c93fdf3825a52a5c9d861873cbd9fbdfe4fcba
Astaroth851 <142568537+Astaroth851@users.noreply.github.com>
2024-05-07 16:40:17 -0500

    Update MariaDbUtil.java

--- src/main/java/com/aca/RyanBatesSound/dao/MariaDbUtil.java
+++ src/main/java/com/aca/RyanBatesSound/dao/MariaDbUtil.java
@@ -1,41 +1,41 @@
 package com.aca.RyanBatesSound.dao;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
 
+
 public class MariaDbUtil {
 
 	private static String connectionUrl = 
-			"jdbc:mongodb://atlas-sql-6579308135cbe34439fefa5f-drchd.a.query.mongodb.net/SoundMovies?ssl=true&authSource=admin";
+			"jdbc:mysql://aws.connect.psdb.cloud?user=4f9uh2pyirhted8w97bb&password=pscale_pw_miOLyGEkrKWSRZbWoLsgXYID9nxJLwXvR5IBSMS4Q6g";
 	
-	public static Connection getConnection() {
-		Connection connection = null;
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://aws.connect.psdb.cloud/soundmovies?sslMode=VERIFY_IDENTITY",
- 	 "9b9a17nrr8z3vzhd72ec",
-  	"************");
-		} catch (ClassNotFoundException e) {
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+        	Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(connectionUrl);
+        } catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-			
-		return connection;
-	}
-	
-	public static void main (String[] args) throws SQLException {
-		Connection connection = MariaDbUtil.getConnection();
+        } catch (SQLException e) {
+            // Print the exception for diagnosis
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void main(String[] args) {
+    	Connection connection = MariaDbUtil.getConnection();
 		if (null != connection) {
 			System.out.println("A real connection");
-		
 		} else 
-			
 			System.out.println("Help. Connection is null.");
 		}
+        
+    }
 
-	}
 
+        
+    
+    		
