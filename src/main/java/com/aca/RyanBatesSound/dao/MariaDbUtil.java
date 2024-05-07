bf30845b5c3ef24d14a8e00018dc2ba283b07866
Astaroth851 <142568537+Astaroth851@users.noreply.github.com>
2024-05-07 16:54:38 -0500

    Update MariaDbUtil.java

--- src/main/java/com/aca/RyanBatesSound/dao/MariaDbUtil.java
+++ src/main/java/com/aca/RyanBatesSound/dao/MariaDbUtil.java
@@ -8,30 +8,6 @@ import java.sql.SQLException;
 
 public class MariaDbUtil {
 
-	private static String connectionUrl = 
-			"jdbc:mysql://aws.connect.psdb.cloud?user=470mk5w1p8dlyunpclsl&password=pscale_pw_bhCA4sOnDToGG083ijOvgXP0b36tp3F35NP1mLwC1bf";
-	
-    public static Connection getConnection() {
-        Connection connection = null;
-        try {
-        	Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection(connectionUrl);
-        } catch (ClassNotFoundException e) {
-			e.printStackTrace();
-        } catch (SQLException e) {
-            // Print the exception for diagnosis
-            e.printStackTrace();
-        }
-        return connection;
-    }
-
-    public static void main(String[] args) {
-    	Connection connection = MariaDbUtil.getConnection();
-		if (null != connection) {
-			System.out.println("A real connection");
-		} else 
-			System.out.println("Help. Connection is null.");
-		}
         
     }
 
