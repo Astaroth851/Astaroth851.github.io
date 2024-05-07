3e5c966f16e40442dfa46368dd971aaa5b07fc28
Astaroth851 <142568537+Astaroth851@users.noreply.github.com>
2024-05-07 16:50:27 -0500

    Update MariaDbUtil.java

--- src/main/java/com/aca/RyanBatesSound/dao/MariaDbUtil.java
+++ src/main/java/com/aca/RyanBatesSound/dao/MariaDbUtil.java
@@ -1,41 +1,41 @@
 package com.aca.RyanBatesSound.dao;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
 
+
 public class MariaDbUtil {
 
 	private static String connectionUrl = 
-			"jdbc:mariadb://localhost:3306/soundmovies?user=root&password=password";
+			"jdbc:mysql://aws.connect.psdb.cloud?user=470mk5w1p8dlyunpclsl&password=pscale_pw_bhCA4sOnDToGG083ijOvgXP0b36tp3F35NP1mLwC1bf";
 	
-	public static Connection getConnection() {
-		Connection connection = null;
-		try {
-			Class.forName("org.mariadb.jdbc.Driver");
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+        	Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection(connectionUrl);
-		} catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-			
-		return connection;
-	}
-	
-	public static void main (String[] args) throws SQLException {
-		Connection connection = MariaDbUtil.getConnection();
+        } catch (SQLException e) {
+            // Print the exception for diagnosis
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void main(String[] args) {
+    	Connection connection = MariaDbUtil.getConnection();
 		if (null != connection) {
 			System.out.println("A real connection");
-		
 		} else 
-			
 			System.out.println("Help. Connection is null.");
 		}
-	}
+        
+    }
 
 
+        
     
     		
