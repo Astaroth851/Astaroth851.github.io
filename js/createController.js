da6851c7d7797f3235ffc76ef3d5994a6c7e8a42
Astaroth851 <142568537+Astaroth851@users.noreply.github.com>
2024-05-10 16:20:25 -0500

    Update createController.js

--- js/createController.js
+++ js/createController.js
@@ -2,7 +2,40 @@
 	var soundapp = angular.module('soundapp');
 	soundapp.controller('createController', function($scope, $http) {
 		$scope.productions = ['Short','Documentary','Feature','Commercial','Podcast','Concept','Corporate','ENG'];
+		
+		
+		
 		$scope.createMovie = function() {
+			
+		  const file = document.getElementById('fileUploadField').files[0];
+		  console.log('File:', file);
+		  const fileReader = new FileReader();
+		  
+		  		// event handler that is called with the load event is fired
+		  fileReader.onload = function(event) {      
+			  var data = fileReader.result;      
+			  if (data !== undefined && data.length > 0) { 
+				       // data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAZkAAAHVCAIAAACs 
+				       // XRGOAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjw...      
+				       // remove the 'data' name/value      
+				       $scope.movie.image = data.split(',')[1];
+				 } else {      
+					 $scope.movie.image = '';
+				 }      
+				 $scope.postMovie();    
+				 }    
+				 // when the readAsDataURL is finished, then the onload event is called 
+				 if (file !== undefined) {      
+					fileReader.readAsDataURL(file);    
+					} else {
+						$scope.movie.image = '';
+						$scope.postMovie()
+						}
+		}
+		
+		$scope.postMovie = function() {
+			console.log('postMovie function called');
+			 console.log('Sending HTTP POST request to create movie');
 			$http.post("/RyanBatesSound/webapi/movies", $scope.movie)
 				.then(function(response) {
 					$scope.createStatus = 'create successful';
@@ -12,11 +45,19 @@
 					console.log('error http POST movies: ' + response.status);
 				});
 		}
+		
+		
+		
+		
+		
 		$scope.clear = function() {
+			
+			console.log('clear function called');
 			$scope.movie.title = '';
 			$scope.movie.releaseYear = '';
 			$scope.movie.production = '';
 			$scope.movie.link = '';
+			$scope.movie.image = '';
 			
 			$scope.createForm.$setUntouched();
 			$scope.createForm.$setPristine();
