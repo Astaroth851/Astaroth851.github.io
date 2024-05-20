3caccd3b36d7001993eba0fd086fca37de5e1c14
Astaroth851 <142568537+Astaroth851@users.noreply.github.com>
2024-05-20 18:10:34 -0500

    Add files via upload

---
+++ src/main/java/com/aca/RyanBatesSound/controller/LogoutServlet.java
@@ -0,0 +1,21 @@
+package com.aca.RyanBatesSound.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Invalidate the session
+        request.getSession().invalidate();
+        // Redirect to login page or home page
+        response.sendRedirect("login.html");
+    }
+}
+
+
