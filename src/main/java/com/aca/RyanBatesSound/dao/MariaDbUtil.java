6b8ef519504f6e16ed9122dcd45c86dd32224bc3
Astaroth851 <142568537+Astaroth851@users.noreply.github.com>
2024-05-21 12:09:55 -0500

    Update MariaDbUtil.java

--- src/main/java/com/aca/RyanBatesSound/dao/MariaDbUtil.java
+++ src/main/java/com/aca/RyanBatesSound/dao/MariaDbUtil.java
@@ -1,55 +1,49 @@
 package com.aca.RyanBatesSound.dao;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-import java.io.File;
 import io.github.cdimascio.dotenv.Dotenv;
-
-
 
 public class MariaDbUtil {
 
-	private static String connectionUrl = 
-			"jdbc:mysql://";
-	
+    private static String connectionUrl = "jdbc:mysql://";
+
     public static Connection getConnection() {
-    	 Dotenv dotenv = Dotenv.configure().directory("C:\\Users\\Astar\\OneDrive\\eclipse-workspace\\RyanBatesSound").load();
-   	  
-    	Connection connection = null;
+        Dotenv dotenv = Dotenv.configure().directory("C:\\Users\\Astar\\OneDrive\\eclipse-workspace\\RyanBatesSound").load();
+
+        Connection connection = null;
         try {
-        	Class.forName("com.mysql.cj.jdbc.Driver");
-        	String dbHost = dotenv.get("DATABASE_HOST");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String dbHost = dotenv.get("DATABASE_HOST");
             String dbUsername = dotenv.get("DATABASE_USERNAME");
             String dbPassword = dotenv.get("DATABASE_PASSWORD");
-             String urlWithCredentials = connectionUrl + dbHost + "?user=" + dbUsername + "&password=" + dbPassword;
-             
-             Properties props = new Properties();
-             props.setProperty("user", dbUsername);
-             props.setProperty("password", dbPassword);
-             props.setProperty("useSSL", "true"); // Enable SSL
+            String dbName = dotenv.get("DATABASE_NAME");
+            String urlWithDatabase = connectionUrl + dbHost + "/" + dbName + "?useSSL=false&serverTimezone=UTC";
 
-			connection = DriverManager.getConnection(urlWithCredentials,props);
+            Properties props = new Properties();
+            props.setProperty("user", dbUsername);
+            props.setProperty("password", dbPassword);
+
+            connection = DriverManager.getConnection(urlWithDatabase, props);
         } catch (ClassNotFoundException e) {
-			e.printStackTrace();
+            e.printStackTrace();
         } catch (SQLException e) {
-            // Print the exception for diagnosis
             e.printStackTrace();
         }
         return connection;
     }
 
     public static void main(String[] args) {
-    	Connection connection = MariaDbUtil.getConnection();
-		if (null != connection) {
-			System.out.println("A real connection");
-		} else 
-			System.out.println("Help. Connection is null.");
-		}
-        
+        Connection connection = MariaDbUtil.getConnection();
+        if (connection != null) {
+            System.out.println("A real connection");
+        } else {
+            System.out.println("Help. Connection is null.");
+        }
     }
-
-
+}
         
     
     		
