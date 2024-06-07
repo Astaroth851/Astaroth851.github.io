9efe14d33dac030a74a906b2e74eb8fc7cb44a3d
Astaroth851 <142568537+Astaroth851@users.noreply.github.com>
2024-06-06 22:39:53 -0500

    Update CorsFilter.java

--- src/main/java/com/aca/RyanBatesSound/controller/CorsFilter.java
+++ src/main/java/com/aca/RyanBatesSound/controller/CorsFilter.java
@@ -1,7 +1,11 @@
+
+Your existing CORS filter is already configured to allow all origins with "Access-Control-Allow-Origin": "*" which is a good start. However, to ensure full CORS support, especially for HTTP methods other than GET and POST, and to handle credentials if necessary, you might want to expand it. Here's an enhanced version of your CORS filter:
+
+java
+Copy code
 package com.aca.RyanBatesSound.controller;
 
 import java.io.IOException;
-
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
@@ -10,10 +14,12 @@ import jakarta.ws.rs.ext.Provider;
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
 
-	@Override
+    @Override
     public void filter(ContainerRequestContext requestContext, 
-      ContainerResponseContext responseContext) throws IOException {
-          responseContext.getHeaders().add(
-            "Access-Control-Allow-Origin", "*");      
+                       ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
 }
